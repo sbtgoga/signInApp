1f7e16a75d5b295811ecde30ef0be9eb3ac90dae
Sebastian Gonzalez <sbt.goga@gmasil.com>
2025-05-02 11:48:08 -0400

    First View is finished

--- app/src/main/java/com/cinncinatiai/signinapplication/MainActivity.kt
+++ app/src/main/java/com/cinncinatiai/signinapplication/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.cinncinatiai.signinapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val presenter: MainPresenter by lazy {
+        MainPresenter()
+    }
+
+
+    private val userText: EditText by lazy {
+        findViewById(R.id.user_text)
+    }
+    private val passwordText: EditText by lazy {
+        findViewById(R.id.password_text)
+    }
+    private val signinButton: Button by lazy {
+        findViewById(R.id.submit_button)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        signinButton.setOnClickListener {
+            val email: String = userText.text.toString()
+            val password: String = passwordText.text.toString()
+            Log.i("MainActivity", "${email} and ${password}")
+
+            if (presenter.signInGranted(email, password)) {
+                TODO()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Give me a valid user and password",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/cinncinatiai/signinapplication/MainPresenter.kt
+++ app/src/main/java/com/cinncinatiai/signinapplication/MainPresenter.kt
@@ -1,4 +1,27 @@
 package com.cinncinatiai.signinapplication
 
 class MainPresenter {
+
+    fun signInGranted(email: String, password: String): Boolean {
+        if (emailValidation(email) && passwordValidation(password)){
+            return true
+        } else{
+            return false
+        }
+    }
+
+    fun emailValidation(email: String): Boolean {
+        val checkAt: Boolean = "@" in email
+        val checkCom: Boolean = ".com" in email
+        return (checkAt && checkCom)
+    }
+
+    fun passwordValidation(password: String): Boolean {
+        if (password.length >= 8) {
+            return true
+        } else{
+            return false
+        }
+    }
+
 }
